cb7aaff6badea96eb6484420c55fd022210aaf37
Jyotish98 <Jyotishsamal980@gmail.com>
2021-08-24 10:38:29 +0530

    new

---
+++ loose_coupling/loose_coupling/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace loose_coupling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Veichle c = new Bike();
+            c.start();
+            Console.ReadLine();
+        }
+    }
+    public interface Veichle
+    {
+        public void start();
+    }
+    public class car : Veichle
+    {
+        public void start()
+        {
+            Console.WriteLine("Car has started");
+        }
+    }
+    public class Bike : Veichle
+    {
+        public void start()
+        {
+            Console.WriteLine(" bike has started");
+        }
+    }
+}
