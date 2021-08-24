d358b4fcada8f7f2e1a5ca16b06122ee9c833b01
Jyotish98 <Jyotishsamal980@gmail.com>
2021-08-24 10:19:00 +0530

    new

---
+++ Team-2-OnlineCourierManagement/Team-2-OnlineCourierManagement/Entities/DeliveryExecutive.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team_2_OnlineCourierManagement.Entities
+{
+    [Table("DeliveryExecutives")] //DeliveryExecutives Table
+    public class DeliveryExecutive:Person
+    {
+        [Key]
+        public int DeliveryExecitiveId { get; set; } //Primary Key
+    }
+}
---
+++ Team-2-OnlineCourierManagement/Team-2-OnlineCourierManagement/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team_2_OnlineCourierManagement.Entities
+{
+    [Table("Users")] //Users Table
+    public class User:Person
+    {
+        [Key]
+        public int UserId { get; set; }  //Primary Key
+    }
+}
